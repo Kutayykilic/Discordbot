35c7b30d6cb2c18c141a98080c3c40766c1f46d0
Kutayykilic <kutayykilic@gmail.com>
2023-02-13 16:50:03 +0300

    Images are saved by the right name after the command

--- bot.py
+++ bot.py
@@ -27,11 +27,13 @@ def run_discord_bot():
         print(message)
         tracemalloc.start()
         user_message=str(message.content)
-        user_message=str.split(user_message," ")
-        user_message=user_message[1]
         print(user_message)
-        if user_message=="!upload" and message.attachments:
-            save_name="image.jpeg"
+        user_message=str.split(user_message," ")
+        command=user_message[1]
+        char_name=user_message[2]
+        print(user_message,char_name)
+        if command=="!upload" and message.attachments:
+            save_name=str(char_name+".jpeg")
             print("here")
             attachment = message.attachments[0]
             await attachment.save(save_name)
